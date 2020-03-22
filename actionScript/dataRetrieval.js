9da22cc914efce66b827907da0716e43fc0c07da
Maanuj Vora <maanujvora@gmail.com>
2020-03-22 04:07:24 -0700

    Update dataRetrieval.js

--- actionScript/dataRetrieval.js
+++ actionScript/dataRetrieval.js
@@ -9,7 +9,7 @@ const MAIN_REPO = "main";
 const outputPath = path.join(WORKSPACE, MAIN_REPO, "docs", "data.json");
 
 let d = new Date();
-const outputPathDaily = path.join(WORKSPACE, MAIN_REPO, "docs", (d.toLocaleDateString() + ".json").replace("/","-"));
+const outputPathDaily = path.join(WORKSPACE, MAIN_REPO, "docs", ((d.toLocaleDateString() + ".json").replace("/","-")).replace("/","-"));
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const https_1 = require("https");
@@ -22,10 +22,10 @@ const get = new Promise((resolve, reject) => {
             const data = JSON.parse(da);
             resolve(data);
             fs.writeFileSync(outputPath, JSON.stringify(data, null, 2));
-            fs.writeFile(outputPathDaily, JSON.stringify(data, null, 2), function (err) {
+            fs.writeFile(outputPathDaily, JSON.stringify(data, null, 2), { flag: 'wx' }, function (err) {
                 if (err) throw err;
-                console.log('Replaced!');
-              });
+                console.log("It worked?");
+            });
         });
     });
     req.end();
