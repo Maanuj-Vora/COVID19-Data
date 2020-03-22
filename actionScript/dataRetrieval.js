96d3cc3708478e617b5fc8bd8e246642e50542b6
Maanuj Vora <maanujvora@gmail.com>
2020-03-22 02:49:31 -0700

    Testing out code

--- actionScript/dataRetrieval.js
+++ actionScript/dataRetrieval.js
@@ -7,6 +7,7 @@ const DATA_REPO = "data";
 const MAIN_REPO = "main";
 
 const outputPath = path.join(WORKSPACE, MAIN_REPO, "docs", "data.json");
+const outputPath1 = path.join(WORKSPACE, MAIN_REPO, "docs", "displayName.txt");
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const https_1 = require("https");
@@ -24,3 +25,59 @@ const get = new Promise((resolve, reject) => {
     req.end();
 });
 module.exports = get;
+
+
+function getAllData() {
+    fetch("https://maanuj-vora.github.io/Bing-COVID-19-Current-Data/data.json")
+        .then(response => response.json())
+        .then(data => {
+            var displayName = [];
+            var totalConfirmed = [];
+            var totalDeaths = [];
+            var totalRecovered = [];
+            var lastUpdated = [];
+            var lat = [];
+            var long = [];
+            var parentId = [];
+
+            displayName.push(data.displayName);
+            totalConfirmed.push(data.totalConfirmed);
+            totalDeaths.push(data.totalDeaths);
+            totalRecovered.push(data.totalRecovered);
+            lastUpdated.push(data.lastUpdated);
+            lat.push(data.lat);
+            long.push(data.long);
+            parentId.push(data.parentId);
+
+            for (y = 0; y < data["areas"].length; y++) {
+
+                displayName.push(data["areas"][y].displayName);
+                totalConfirmed.push(data["areas"][y].totalConfirmed);
+                totalDeaths.push(data["areas"][y].totalDeaths);
+                totalRecovered.push(data["areas"][y].totalRecovered);
+                lastUpdated.push(data["areas"][y].lastUpdated);
+                lat.push(data["areas"][y].lat);
+                long.push(data["areas"][y].long);
+                parentId.push(data["areas"][y].parentId);
+
+                if (data["areas"][y]["areas"].length != 0) {
+                    for (z = 0; z < data["areas"][y]["areas"].length; z++) {
+
+                        displayName.push(data["areas"][y]["areas"][z].displayName);
+                        totalConfirmed.push(data["areas"][y]["areas"][z].totalConfirmed);
+                        totalDeaths.push(data["areas"][y]["areas"][z].totalDeaths);
+                        totalRecovered.push(data["areas"][y]["areas"][z].totalRecovered);
+                        lastUpdated.push(data["areas"][y]["areas"][z].lastUpdated);
+                        lat.push(data["areas"][y]["areas"][z].lat);
+                        long.push(data["areas"][y]["areas"][z].long);
+                        parentId.push(data["areas"][y]["areas"][z].parentId);
+
+                    }
+                }
+            }
+
+            fs.writeFile('displayName.txt', displayName, (err) => {
+                if (err) throw err;
+            })
+        });
+}
