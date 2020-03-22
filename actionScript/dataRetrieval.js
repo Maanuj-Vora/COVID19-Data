ad3a30244259656356d4e0e4ce04048b34c556fc
Maanuj Vora <maanujvora@gmail.com>
2020-03-22 04:00:00 -0700

    Update dataRetrieval.js

--- actionScript/dataRetrieval.js
+++ actionScript/dataRetrieval.js
@@ -9,7 +9,7 @@ const MAIN_REPO = "main";
 const outputPath = path.join(WORKSPACE, MAIN_REPO, "docs", "data.json");
 
 let d = new Date();
-const outputPathDaily = path.join(WORKSPACE, MAIN_REPO, "docs", d.toLocaleDateString()+".json");
+const outputPathDaily = path.join(WORKSPACE, MAIN_REPO, "docs", (d.toLocaleDateString() + ".json").replace("/","-"));
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const https_1 = require("https");
